e15bd2bacd745ea58198e1f635ee0858bfd62d49
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-31 23:08:05 +0800

    use while True loop to continously ask user for a valid input and then use strip to remove leading spaces. Then use break to end loop if the input is already valid

--- batch_5/print_the_number_of_words_in_the_input.py
+++ batch_5/print_the_number_of_words_in_the_input.py
@@ -1,6 +1,11 @@
 # define the function that will count the number of words in an input
 def count_words():
 # use loop
+    while True:
+        sentence = input("Enter a sentence:")
+        if sentence.strip():
+            break
+    print("Please enter a valide sentence:")
 # ask for user input 
 # count the number of words in the input 
 # print the amount of words 
