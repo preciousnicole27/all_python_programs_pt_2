af4b12176bb30fdb357d683faf15b7bfafbd4db9
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-31 23:03:54 +0800

    define the function that will count the number of words in an input

--- batch_5/print_the_number_of_words_in_the_input.py
+++ batch_5/print_the_number_of_words_in_the_input.py
@@ -1,5 +1,8 @@
 # define the function that will count the number of words in an input
+def count_words():
 # use loop
 # ask for user input 
 # count the number of words in the input 
 # print the amount of words 
+
+count_words()
