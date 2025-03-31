d4ae0a9f473667e20fecd71f61e6e7066648e129
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-31 22:19:16 +0800

    if full name is valid, input is evaluated to true and then breaks to exit the loop

--- batch_5/print_full_name_in_proper_casing.py
+++ batch_5/print_full_name_in_proper_casing.py
@@ -1,7 +1,10 @@
 # define the function that will fixed the improper casing to proper casing
 def proper_case():
+# use loop
     while True:
 # ask user to input an input with improper casing
-# use loop
+        full_name = input("Enter your full name in improper casing:")
+        if full_name.strip():
+            break
 # turn the input to proper cased letters
 # print the results 
