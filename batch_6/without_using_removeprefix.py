faa1b8562be729807a45997a6b9f5fee9447e7db
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-01 02:39:29 +0800

    check if the input starts with a prefix

--- batch_6/without_using_removeprefix.py
+++ batch_6/without_using_removeprefix.py
@@ -3,6 +3,7 @@ def remove_prefix(text, prefix):
 # init index
     index = 0
 # check if input starts with a prefix
+    while index < len(prefix) and index < len(text) and text [index]:
 # use slicing 
 # ask for user input
 # print input with the removed prefix
