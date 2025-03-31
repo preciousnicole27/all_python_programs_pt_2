30a071703293c12ef22a16d5c6878ce073814b7b
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-31 23:16:46 +0800

    separated the sentences using split(), then count the separated elements using len()

--- batch_5/print_the_number_of_words_in_the_input.py
+++ batch_5/print_the_number_of_words_in_the_input.py
@@ -1,13 +1,14 @@
 # define the function that will count the number of words in an input
 def count_words():
 # use loop
+# ask user for input
     while True:
         sentence = input("Enter a sentence:")
         if sentence.strip():
             break
-    print("Please enter a valide sentence:")
-# ask for user input 
-# count the number of words in the input 
+    print("Please enter a valide sentence:") 
+# count the number of words in the input
+    word_count = len(sentence.split())
 # print the amount of words 
 
 count_words()
