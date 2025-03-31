466c33b92d905fca156019302180805894d60231
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-31 16:40:29 +0800

    used break to exit the loop if the inpur is invalid and then ask user to input a valid full name

--- batch_5/print_full_name_without_several_spaces.py
+++ batch_5/print_full_name_without_several_spaces.py
@@ -3,5 +3,11 @@
 def remove_leading_spaces():
 # use while loop to ask for input
     while True:
+        full_name = input("Enter your full name:")
+        if full_name.strip():
+            break
+        print("Please input a valid full name:")
 # print if the input is invalid
 # display the full name without the leading spaces
+
+remove_leading_spaces()
