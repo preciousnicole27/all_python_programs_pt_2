2bb7c8c378dc2fbbbe11e28b01ddc1ab624903b1
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-31 21:30:24 +0800

    define the function that will convert the improper casing into proper casing and then use while loop to continously ask user for a valid input

--- batch_5/print_full_name_in_proper_casing.py
+++ batch_5/print_full_name_in_proper_casing.py
@@ -1,4 +1,6 @@
 # define the function that will fixed the improper casing to proper casing
+def proper_case():
+    while True:
 # ask user to input an input with improper casing
 # use loop
 # turn the input to proper cased letters
