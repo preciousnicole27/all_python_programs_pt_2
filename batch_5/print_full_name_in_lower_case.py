b85d4db7ef79c3be2abd04c2c640a710879d27a8
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-31 21:07:12 +0800

    use while true loop to monitor if the input is valid. Then use strip function to remove spaces then use break to stop the loop if the input is valid

--- batch_5/print_full_name_in_lower_case.py
+++ batch_5/print_full_name_in_lower_case.py
@@ -1,7 +1,10 @@
 # define the function that will convert the input into lower case
 def lower_case():
-    
 # ask for user input 
 # use while True loop
+    while True:
+        full_name = input("Enter your full name:")
+        if full_name.strip():
+            break
 # turn input into lower case letters
 # print full name in lower case
