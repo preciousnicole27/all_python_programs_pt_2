369bd3e43581c3dc5cf1c1b13db7c8c745a7e5ae
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-01 01:52:17 +0800

    define the function

--- batch_5/print_full_name_in_pascal_case.py
+++ batch_5/print_full_name_in_pascal_case.py
@@ -1,5 +1,8 @@
 # define the function that will convert improper casing to pascal casing
+def pascal_case():
 # use loop
 # ask for user input in incorrect casing
 # turn input into pascal casing
 # print the results
+
+pascal_case()
