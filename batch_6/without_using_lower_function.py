3e61e2f5d6ce8924236aeeb522376091a8cae5bd
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-01 03:10:33 +0800

    used casefold to convert input into lowercase and returns the text in small letters

--- batch_6/without_using_lower_function.py
+++ batch_6/without_using_lower_function.py
@@ -1,6 +1,7 @@
 # define the function
 def convert_to_lowercase_letters(text):
 # convert the input into lowercase letters
+    return text.casefold()
 # ask for user input
 text = input("Enter an input:")
 # print the result
