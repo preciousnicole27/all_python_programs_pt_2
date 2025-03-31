2a27a3530f4a04e769ad5cee1d2dfc5a78d24264
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-01 02:41:43 +0800

    ask for user input

--- batch_6/without_using_removeprefix.py
+++ batch_6/without_using_removeprefix.py
@@ -5,5 +5,8 @@ def remove_prefix(text, prefix):
 # check if input starts with a prefix
     while index < len(prefix) and index < len(text) and text [index]:
 # use slicing 
+   
 # ask for user input
+text = input("Enter a number:")
+prefix = input("Enter the prefix:")
 # print input with the removed prefix
