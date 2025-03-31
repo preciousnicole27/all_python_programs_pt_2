0357860f9d071b9fb938e0be1bc43db9167fa5d7
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-31 17:36:58 +0800

    added a pseudocode

---
+++ batch_5/print_number_in_six_digit_format.py
@@ -0,0 +1,4 @@
+# define the function that will convert the number to 6 digits with leading zeroes
+# ask for user input
+# format the number to 6 digits with leading zero
+# print the number in six digit format
