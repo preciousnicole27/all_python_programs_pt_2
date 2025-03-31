899aa7a7b0d5a4b607f508c93d42e0ee9e547f2d
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-31 21:24:20 +0800

    added a pseudocode

---
+++ batch_5/print_full_name_in_proper_casing.py
@@ -0,0 +1,5 @@
+# define the function that will fixed the improper casing to proper casing
+# ask user to input an input with improper casing
+# use loop
+# turn the input to proper cased letters
+# print the results 
