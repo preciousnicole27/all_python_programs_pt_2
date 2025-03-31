ce236fe0d820e460c97533ca46b8dcee1b7faaf1
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-31 17:39:03 +0800

    define the function six digit format to format the input into a six digit format

--- batch_5/print_number_in_six_digit_format.py
+++ batch_5/print_number_in_six_digit_format.py
@@ -1,4 +1,7 @@
 # define the function that will convert the number to 6 digits with leading zeroes
+def six_digit_format():
 # ask for user input
 # format the number to 6 digits with leading zero
 # print the number in six digit format
+
+six_digit_format()
