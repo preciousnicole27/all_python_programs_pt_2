cf31d1f4706301c83e70251b97d8613c35da3611
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-01 03:21:13 +0800

    use return True if there are no lowercase letters

--- batch_6/without_using_isupper_function.py
+++ batch_6/without_using_isupper_function.py
@@ -4,7 +4,8 @@ def to_all_uppercase_letter(text):
     for char in text:
         if "a" <= char <= "z":
 # use return
-
+            return False
+        return True
 # ask for user input
 text = input("Enter an input:")
 # print the result
