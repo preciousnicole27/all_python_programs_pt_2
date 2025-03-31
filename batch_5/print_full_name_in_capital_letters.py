cb41ae4b802d57dfe7ba07a73bd61acf6f65a512
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-31 19:08:07 +0800

    if the user enters valid input the program proceeds, if the prinput is invalid the program then ask for a valid input then continues the loop

--- batch_5/print_full_name_in_capital_letters.py
+++ batch_5/print_full_name_in_capital_letters.py
@@ -4,6 +4,8 @@ def capital_letters():
 # use while true loop 
     while True:
         full_name = input("Enter your full name:")
-        
+        if full_name.strip():
+            break
+        print("Please input a valid name:")
 # turn input into capital letters
 # print full name in capital letters
