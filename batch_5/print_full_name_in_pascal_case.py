7bbd8f1b64bb248c83b56caaa811ea84ba6152f2
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-01 01:54:44 +0800

    use loop then check if input is valid or not

--- batch_5/print_full_name_in_pascal_case.py
+++ batch_5/print_full_name_in_pascal_case.py
@@ -1,7 +1,13 @@
 # define the function that will convert improper casing to pascal casing
 def pascal_case():
 # use loop
+    while True:
 # ask for user input in incorrect casing
+        full_name = input("Enter your full name:")
+        if full_name.strip():
+            break
+        else:
+            print("Please put a valid input:")
 # turn input into pascal casing
 # print the results
 
