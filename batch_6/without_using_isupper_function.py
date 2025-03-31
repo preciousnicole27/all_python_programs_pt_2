5e1aa84fa74b781cfd15debc576930f0e81341d5
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-01 03:19:53 +0800

    added a semicolon

--- batch_6/without_using_isupper_function.py
+++ batch_6/without_using_isupper_function.py
@@ -2,8 +2,9 @@
 def to_all_uppercase_letter(text):
 # find any lowercase letter
     for char in text:
-        if "a" <= char <= "z"
+        if "a" <= char <= "z":
 # use return
+
 # ask for user input
 text = input("Enter an input:")
 # print the result
