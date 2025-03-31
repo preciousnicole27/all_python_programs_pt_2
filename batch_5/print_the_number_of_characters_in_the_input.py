dfaa50a2e2a0725845da0ce12e5d2ff1e667b1ba
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-31 23:55:21 +0800

    define the function that will count the number of characters in an input

--- batch_5/print_the_number_of_characters_in_the_input.py
+++ batch_5/print_the_number_of_characters_in_the_input.py
@@ -1,5 +1,9 @@
 # define the function that will count the number of characters in the input
+def count_characters():
+
 # use loop
 # ask user for input
 # count the number of characters un the input
 # print the number of characters
+
+count_characters()
