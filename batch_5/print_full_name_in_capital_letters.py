3c71d5de71848d620e390ee2645dda4452febf70
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-31 18:55:05 +0800

    added a pseudocode

---
+++ batch_5/print_full_name_in_capital_letters.py
@@ -0,0 +1,5 @@
+# define the function that will convert input into capital letters
+# ask for user input
+# use while true loop 
+# turn input into capital letters
+# print full name in capital letters
