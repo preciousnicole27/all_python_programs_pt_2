fbe63cf9c129ee271db922460e3debd8eadbd4aa
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-01 00:58:06 +0800

    use loop to repeatedly ask user until inpurt is valid. then use strip() to remove spaces. use break to stop the loop if input is already valid

--- batch_5/print_the_number_of_characters_in_the_input.py
+++ batch_5/print_the_number_of_characters_in_the_input.py
@@ -1,9 +1,15 @@
 # define the function that will count the number of characters in the input
 def count_characters():
-
 # use loop
 # ask user for input
+    while True:
+        full_name = ("Enter your full name:")
+        if full_name.strip():
+            break
+        else:
+            print("Please input a valid name:")
 # count the number of characters un the input
+
 # print the number of characters
 
 count_characters()
