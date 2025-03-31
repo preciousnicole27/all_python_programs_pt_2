e6ed46549f633ac2445118372e48746b546fc113
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-31 16:35:35 +0800

    defined the function, and added a loop

--- batch_5/print_full_name_without_several_spaces.py
+++ batch_5/print_full_name_without_several_spaces.py
@@ -1,4 +1,7 @@
 # ask for user input 
 # define the function that remove the leading spaces
+def remove_leading_spaces():
+# use while loop to ask for input
+    while True:
 # print if the input is invalid
 # display the full name without the leading spaces
