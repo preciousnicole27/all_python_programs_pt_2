2018f4421346528472fbd58bec250fa23af89052
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-01 02:15:08 +0800

    equate index to 0 to begin from the very 1st position, then use while loop to move the index function and find the first non spaced input

--- batch_6/without_using_lstrip_function.py
+++ batch_6/without_using_lstrip_function.py
@@ -1,7 +1,9 @@
 # process and define the input to remove leading spaces
 def remove_leading_spaces(text):
 # use loop
+    index = 0
 # init index
+    while index < len(text) and text [index] == " ":
 # use slicing
 # get user input 
 # print results
