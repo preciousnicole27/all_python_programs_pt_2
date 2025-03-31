5f4ed5c2885da4fa78bcf92529dd45989097913d
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-31 19:37:14 +0800

    added a pseudocode

---
+++ batch_5/print_full_name_in_lower_case.py
@@ -0,0 +1,5 @@
+# define the function that will convert the input into lower case
+# ask for user input 
+# use while True loop
+# turn input into lower case letters
+# print full name in lower case
