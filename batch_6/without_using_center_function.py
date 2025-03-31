928998e652ad7b40fde4e83170ff18b13ba6aa23
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-01 04:21:42 +0800

    multiply a space on both left and right spaces to center the text

--- batch_6/without_using_center_function.py
+++ batch_6/without_using_center_function.py
@@ -6,6 +6,7 @@ def center_tex(text, width):
     left_spaces = total_spaces // 2
     right_spaces = total_spaces - left_spaces
 # return text
+    return " " * left_spaces + text + " " * right_spaces
 # ask for user input
 text = input("Enter a text:")
 width = int(input("Enter width:"))
