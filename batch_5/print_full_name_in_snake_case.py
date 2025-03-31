782e063fca21c1fde21abe857da398ba6d971b8f
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-01 02:03:57 +0800

    define the function, then check if input is valid

--- batch_5/print_full_name_in_snake_case.py
+++ batch_5/print_full_name_in_snake_case.py
@@ -1,5 +1,12 @@
 # define the function that will convert input into snake case
+def snake_case():
 # ask user for input
 # use loop
+    while True:
+        full_name = input("Enter your full name:")
+        if full_name.strip():
+            break
+    else:
+        print("Enter a valid name:")
 # convert input using snake casing
 # print full name in snake case
