aff25c89c639502cc6a3b5e5a0a9695587337309
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-31 17:43:55 +0800

    use while true loop to run the program unless an error existed

--- batch_5/print_number_in_six_digit_format.py
+++ batch_5/print_number_in_six_digit_format.py
@@ -1,5 +1,8 @@
 # define the function that will convert the number to 6 digits with leading zeroes
 def six_digit_format():
+# use a while True loop and try 
+    while True:
+        try:
 # ask for user input
 # format the number to 6 digits with leading zero
 # print the number in six digit format
