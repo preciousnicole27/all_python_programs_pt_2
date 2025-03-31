7311c8fe5014e55cef67550369d2409a1a705b89
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-01 03:18:33 +0800

    find if the input has any lowercase letter

--- batch_6/without_using_isupper_function.py
+++ batch_6/without_using_isupper_function.py
@@ -1,6 +1,8 @@
 # define the function
 def to_all_uppercase_letter(text):
 # find any lowercase letter
+    for char in text:
+        if "a" <= char <= "z"
 # use return
 # ask for user input
 text = input("Enter an input:")
