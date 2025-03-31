517a754fd76c1cbe45847d7c89ca016ab8140cbd
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-31 23:53:17 +0800

    added a pseudocode

---
+++ batch_5/print_the_number_of_characters_in_the_input.py
@@ -0,0 +1,5 @@
+# define the function that will count the number of characters in the input
+# use loop
+# ask user for input
+# count the number of characters un the input
+# print the number of characters
