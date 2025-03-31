297acc4f77389b341a998be215e92abf3af1f47f
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-31 22:36:39 +0800

    use loop to continousy prompt user for a valid input then use strip to remove the spaces. Lastly use break to stop the loop if the input is already valid. If the input is still invalid print Invalid input, then continue the loop until the text is valid

--- batch_5/print_full_name_in_reverse_casing.py
+++ batch_5/print_full_name_in_reverse_casing.py
@@ -2,5 +2,10 @@
 def reverse_case():
 # use loop
 # ask user for input
+    while True:
+        full_name = input("Enter your full name:")
+        if full_name.strip()
+        break
+    print("Please input a valid name:")
 # turn input into reverse casing
 # print full name in reverse casing
