fa60d7fd210ebb4a9e850a3cf25af23b8f7654ee
Precious Nicole Dela Cruz <preciousnicoledelacruz.27@gmail.com>
2025-04-01 09:39:40 +0800

    Delete batch_6 directory

--- batch_6/without_using_lower_function.py
+++
@@ -1,8 +0,0 @@
-# define the function
-def convert_to_lowercase_letters(text):
-# convert the input into lowercase letters
-    return text.casefold()
-# ask for user input
-text = input("Enter an input:")
-# print the result
-print(convert_to_lowercase_letters(text))
--- batch_6/without_using_swapcase_function.py
+++
@@ -1,8 +0,0 @@
-# define a function
-def swap_case(text):
-# create an empty string
-# use loop
-# use return
-# ask for user input
-text = ("Enter a text:")
-# print results
